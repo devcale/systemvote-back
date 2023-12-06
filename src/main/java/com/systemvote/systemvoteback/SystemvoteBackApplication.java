e1b1801f7562bdd147aa7920ab815d9966d1c87a
cale <nicolas.calero1@gmail.com>
2023-12-06 12:43:07 -0500

    Initialized app, defined User model

---
+++ src/main/java/com/systemvote/systemvoteback/SystemvoteBackApplication.java
@@ -0,0 +1,13 @@
+package com.systemvote.systemvoteback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemvoteBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemvoteBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/systemvote/systemvoteback/SystemvoteBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systemvote.systemvoteback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemvoteBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
